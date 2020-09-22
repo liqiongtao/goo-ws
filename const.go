37dcdb3f307c6f62760689d1ff8dfb4114350971
liqiongtao <hnatao@126.com>
2020-09-22 23:37:53 +0800

    init

---
+++ const.go
@@ -0,0 +1,11 @@
+package goo_ws
+
+import "time"
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	filePeriod     = 3 * time.Second
+	maxMessageSize = 512
+)
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/liqiongtao/goo-ws
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
